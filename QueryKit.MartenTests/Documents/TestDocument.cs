f75201bce0404edaea70bd74e3c2be08bfd88ced
Lawrence Moorehead <lawrence.moorehead@elemdiscovery.com>
2025-07-15 11:42:00 -0400

    Checking in consolidated Tests.
    Note that this will not update the action to run the Marten specific tests in the pipeline.

---
+++ QueryKit.MartenTests/Documents/TestDocument.cs
@@ -0,0 +1,30 @@
+namespace QueryKit.MartenTests.Documents;
+
+public class TestDocument
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public Guid? RelatedId { get; set; }
+    public decimal Rating { get; set; }
+    public int Age { get; set; }
+    public BirthMonthEnum BirthMonth { get; set; }
+    public DateTimeOffset? SpecificDate { get; set; }
+    public DateOnly? Date { get; set; }
+    public TimeOnly? Time { get; set; }
+}
+
+public enum BirthMonthEnum
+{
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12
+}
